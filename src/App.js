6a39e31104724210b1a8eddca3f976b7b12e3abe
Your Name <controlsltrapidfire@gmail.comm>
2018-01-29 21:36:56 -0600

    entering data to form

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Projects from './components/Projects';
+import AddProject from './components/AddProject';
+import './App.css';
+
+
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state= {
+      projects: []
+}}
+componentWillMount(){
+  this.setState({projects: [
+    {
+      title: "Business Website",
+      category: "Web Design"
+    },
+    {
+      title: "Social App",
+      category: "Mobile Development"
+    },
+    {
+      title: "Eccommerce Shopping Cart",
+      category: "Web Development"
+    }
+  ]})
+}
+
+  render() {
+    return (
+      <div className="App">
+        <AddProject  />
+        <Projects projects={this.state.projects} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/AddProject.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class AddProject extends Component {
+  constructor(){
+    super();
+    this.state = {
+      newProject:{}
+    }
+  }
+
+  static defaultProps ={
+    categories: ['Web Design', 'Web Development', 'Mobile Development']
+  }
+
+  handleSubmit(e) {
+    console.log(this.refs.title.value);
+    e.preventDefault();
+  }
+  render() {
+    let categoryOptions = this.props.categories.map(category =>{
+      return <option key={category} value="category"> {category} </option>
+    });
+    return (
+      <div>
+        <h3>Add Project</h3>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <div>
+            <label>Title</label><br />
+            <input type="text" ref="title" />
+          </div>
+          <div>
+            <label>Category</label><br />
+            <select ref="category">
+              {categoryOptions}
+            </select>
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddProject;
